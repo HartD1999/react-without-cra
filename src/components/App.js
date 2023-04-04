05ad1e9fca5f8c3efb69bf78de6deb3463bb91bf
Artem Himchencko <artem@admins-MacBook-Pro.local>
2023-04-04 23:48:55 +0300

    React app was created without using CRA

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Hello Artem</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
